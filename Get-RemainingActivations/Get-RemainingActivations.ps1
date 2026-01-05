107e1f572434711cce974576bdbc70ca9d6e5c92
Elizabeth Greene <egreene@microsoft.com>
2026-01-05 12:03:59 -0600

    Update Get-RemainingActivations.ps1
    Allow passing arguments from the command line

--- Get-RemainingActivations/Get-RemainingActivations.ps1
+++ Get-RemainingActivations/Get-RemainingActivations.ps1
@@ -1,4 +1,4 @@
-﻿function Get-RemainingActivations {
+﻿#function Get-RemainingActivations {
   <#
     .SYNOPSIS
         Queries Microsoft's Volume Activation service to return the remaining activation count for a given Volume License.
@@ -84,11 +84,10 @@
     } catch {
         return "Error: $($_.Exception.Message)"
     }
-}
+#}
 
 # Example call
-Get-RemainingActivations -ExtendedPID "03612-03763-081-809727-00-1033-14393.0000-3252025"
-
+#Get-RemainingActivations -ExtendedPID "03612-03763-081-809727-00-1033-14393.0000-3252025"
 # As of 20251121, that example returns -1, which indicates unlimited activations remaining.
 
 ## Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment. THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object code form of the Sample Code, provided that. You agree: (i) to not use Our name, logo, or trademarks to market Your software product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which the Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code
